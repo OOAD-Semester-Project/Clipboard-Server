56b70a640bd871360ebc418348141f1b1f3d6fec
Madhusudhan Aithal <madhuaithal@Madhusudhans-MacBook-Pro.local>
2020-04-15 15:09:55 -0600

    Fixed some bugs related to sockets

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors')
 const app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+app.use(cors())
 const MongoClient = require('mongodb').MongoClient;
-const url = "mongodb+srv://admin:admin@clipboard-rtfg2.mongodb.net/test?retryWrites=true&w=majority";
+const dbUrl = "mongodb+srv://admin:admin@cluster0-3fbr6.gcp.mongodb.net/test?retryWrites=true&w=majority";
 let str = "";
 
 const server = require('http').createServer(app);
@@ -32,7 +34,6 @@ app.post('/addClip', (req, res) => {
     dbo.collection("clips").insertOne(myobj, function(err, res) {
         if (err) throw err;
         console.log("1 document inserted");
-        db.close();
         });
     res.send(200);
 });
@@ -41,14 +42,12 @@ app.route('/clips/:userId').get(function(req, res) {
     userId = req.params.userId;
     console.log(userId);
     main_result = {};
-    if (err) throw err;
     var dbo = mongodb.db(dbName);
-    var query = {user: userId};
+    var query = {userId: userId};
     dbo.collection("clips").find(query).toArray(function(err, result) {
       if (err) throw err;
       main_result = result;
-      //console.log(result);
-      db.close();
+    //   console.log(result);
       res.send(result);
     });
     //res.send(main_result);
@@ -65,7 +64,6 @@ app.route('/deleteClip').delete(function(req, res) {
         if (err) throw err;
         result["success"] = true
         result["message"] = "Successfully deleted the document"
-        db.close();
     });
     res.status(200);
     res.send(result);
@@ -73,60 +71,48 @@ app.route('/deleteClip').delete(function(req, res) {
 
 io.on('connection', function(socket) {  
     console.log('Desktop Client connected...');
-    socket.emit('message','you are connected');
+    // socket.emit('message','you are connected');
+    socket.on("join", function(reqObj) {
+        let userId = reqObj["userId"];
+        console.log("User joined: ",userId)
+        socket.join(userId);
+        socket.emit('message','you are added to the room '+userId);
+        console.log(io.sockets.adapter.rooms[userId]);
+    });
+    socket.on("leave", function(reqObj) {
+        let userId = reqObj["userId"];
+        console.log("User left: ",userId)
+        socket.leave(userId);
+        socket.emit('message','you are removed from the room '+userId);
+    });
     socket.on('desktopClient',function(reqObj){
-            let userId = reqObj["userId"];
-            let clipData = reqObj["clipboardText"]
-            var dbo = mongodb.db(dbName);
-            myobj = data;
-            dbo.collection("clips").insertOne(myobj, function(err, res) {
-                if (err) throw err;
-                console.log("1 document inserted");
-                db.close();
-              });
-              socket.to('mobileClient').emit(data,'From Desktop');
+        let userId = reqObj["userId"];
+        let clipData = reqObj["clipboardText"]
+        var dbo = mongodb.db(dbName);
+        myobj = reqObj;
+        dbo.collection("clips").insertOne(myobj, function(err, res) {
+            if (err) throw err;
+            // console.log(res);
+            console.log("1 document inserted");      
+            io.sockets.emit("newData", myobj);      
+        });
+        //   socket.to('mobileClient').emit(clipData,'From Desktop');
     });
 
-    socket.on('mobileClient',function(data){
-            var dbo = mongodb.db(dbName);
-            myobj = data;
-            dbo.collection("clips").insertOne(myobj, function(err, res) {
-                if (err) throw err;
-                console.log("1 document inserted");
-                db.close();
-              });
-              socket.emit('message','From Desktop');
-              socket.to('desktopClient').emit(data,'From Desktop');
+    socket.on('mobileClient',function(reqObj){
+        let userId = reqObj["userId"];
+        let clipData = reqObj["clipboardText"]
+        var dbo = mongodb.db(dbName);
+        myobj = reqObj;
+        dbo.collection("clips").insertOne(myobj, function(err, res) {
+            if (err) throw err;
+            console.log("1 document inserted");
+            io.sockets.emit("newData", myobj);      
+        });
+        // socket.emit('message','From Desktop');
+        // socket.to(userId).emit("newData", clipData);
     });
-    //below code is not needed
-    socket.on('join', function(data) {
-        console.log(data);
-        socket.emit('messages', 'Hello from server');
-    });
+        
 });
-/* 
-
-<script>
- var socket = io.connect();
- socket.on('connect', function(data) {
-    socket.emit('join', 'Hello World from client');
- });
- socket.on('broad', function(data) {
-         $('#future').append(data+ "<br/>");
-   });
-
- $('form').submit(function(e){
-     e.preventDefault();
-     var message = $('#chat_input').val();
-     socket.emit('messages', message);
- });
-</script>
-
-
-*/
 
 server.listen(3000);
-
-
-
-//var server = app.listen(3000, function() {console.log("Server listening on port http://localhost:3000 ");});
