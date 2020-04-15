78a94f0a54920abbb4a90b95c947cf710bbb5292
Madhusudhan Aithal <madhuaithal@Madhusudhans-MacBook-Pro.local>
2020-04-14 20:00:59 -0600

    Added connection pooling to mongodb connection

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
-var mongodb = require('mongodb');
-var MongoClient = mongodb.MongoClient;
-var url = "mongodb+srv://admin:admin@clipboard-rtfg2.mongodb.net/test?retryWrites=true&w=majority";
-var str = "";
+const MongoClient = require('mongodb').MongoClient;
+const url = "mongodb+srv://admin:admin@clipboard-rtfg2.mongodb.net/test?retryWrites=true&w=majority";
+let str = "";
 
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const dbName = "copa-db";
+let mongodb;
 
+MongoClient.connect(dbUrl, {  
+    poolSize: 10
+},function(err, db) {
+        mongodb=db;
+    }
+);
 
 app.use(express.static(__dirname + '/node_modules'));
 app.get('/', function(req, res,next) {
@@ -20,15 +27,13 @@ app.get('/', function(req, res,next) {
 
 app.post('/addClip', (req, res) => {
     console.log('Got body:', req.body);
-    MongoClient.connect(url,function(err,db){
-        var dbo = db.db("clipboard");
-        myobj = req.body;
-        dbo.collection("clips").insertOne(myobj, function(err, res) {
-            if (err) throw err;
-            console.log("1 document inserted");
-            db.close();
-          });
-    });
+    var dbo = mongodb.db(dbName);
+    myobj = req.body;
+    dbo.collection("clips").insertOne(myobj, function(err, res) {
+        if (err) throw err;
+        console.log("1 document inserted");
+        db.close();
+        });
     res.send(200);
 });
 
@@ -36,9 +41,8 @@ app.route('/clips/:userId').get(function(req, res) {
     userId = req.params.userId;
     console.log(userId);
     main_result = {};
-   MongoClient.connect(url, function(err, db) {
     if (err) throw err;
-    var dbo = db.db("clipboard");
+    var dbo = mongodb.db(dbName);
     var query = {user: userId};
     dbo.collection("clips").find(query).toArray(function(err, result) {
       if (err) throw err;
@@ -48,61 +52,56 @@ app.route('/clips/:userId').get(function(req, res) {
       res.send(result);
     });
     //res.send(main_result);
-   });
 });
 
 app.route('/deleteClip').delete(function(req, res) {
-  console.log('Got body DELETE:', req.body);
-  let result = {};
-  MongoClient.connect(url,function(err,db){
-      var dbo = db.db("clipboard");
-      id = req.body._id;
-      console.log("Id to be deleted: "+id)
-      var myquery = { _id: new mongodb.ObjectID(id) };
-      dbo.collection("clips").deleteOne(myquery, function(err, res) {
-          if (err) throw err;
-          result["success"] = true
-          result["message"] = "Successfully deleted the document"
-          db.close();
-        });
-  });
-  res.status(200);
-  res.send(result);
+    console.log('Got body DELETE:', req.body);
+    let result = {};
+    var dbo = mongodb.db(dbName);
+    id = req.body._id;
+    console.log("Id to be deleted: "+id)
+    var myquery = { _id: new mongodb.ObjectID(id) };
+    dbo.collection("clips").deleteOne(myquery, function(err, res) {
+        if (err) throw err;
+        result["success"] = true
+        result["message"] = "Successfully deleted the document"
+        db.close();
+    });
+    res.status(200);
+    res.send(result);
 });
 
-io.on('connection', function(client) {  
+io.on('connection', function(socket) {  
     console.log('Desktop Client connected...');
-    client.emit('message','you are connected');
-    client.on('desktipClient',function(data){
-        MongoClient.connect(url,function(err,db){
-            var dbo = db.db("clipboard");
+    socket.emit('message','you are connected');
+    socket.on('desktopClient',function(reqObj){
+            let userId = reqObj["userId"];
+            let clipData = reqObj["clipboardText"]
+            var dbo = mongodb.db(dbName);
             myobj = data;
             dbo.collection("clips").insertOne(myobj, function(err, res) {
                 if (err) throw err;
                 console.log("1 document inserted");
                 db.close();
               });
-        });
-        client.to('mobileClient').emit(data,'From Desktop');
+              socket.to('mobileClient').emit(data,'From Desktop');
     });
 
-    client.on('mobileClient',function(data){
-        MongoClient.connect(url,function(err,db){
-            var dbo = db.db("clipboard");
+    socket.on('mobileClient',function(data){
+            var dbo = mongodb.db(dbName);
             myobj = data;
             dbo.collection("clips").insertOne(myobj, function(err, res) {
                 if (err) throw err;
                 console.log("1 document inserted");
                 db.close();
               });
-        });
-        client.emit('message','From Desktop');
-        client.to('desktipClient').emit(data,'From Desktop');
+              socket.emit('message','From Desktop');
+              socket.to('desktopClient').emit(data,'From Desktop');
     });
     //below code is not needed
-    client.on('join', function(data) {
+    socket.on('join', function(data) {
         console.log(data);
-        client.emit('messages', 'Hello from server');
+        socket.emit('messages', 'Hello from server');
     });
 });
 /* 
