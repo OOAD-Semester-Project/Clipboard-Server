b736de95af4a4229218d30e2f1c21ea9c6c516a8
Madhusudhan Aithal <madhuaithal@Madhusudhans-MacBook-Pro.local>
2020-04-26 23:13:06 -0600

    Added deleteClips endpoint

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ let keycloak = new Keycloak({ store: memoryStore });
 const PORT = process.env.PORT || 3000;
 const USERINFO_ENDPOINT = "https://copa-keycloak.herokuapp.com/auth/realms/copa/protocol/openid-connect/userinfo"
 const jwtDecode = require('jwt-decode');
+const ObjectId = require('mongodb').ObjectID;
 
 //session
 app.use(session({
@@ -52,18 +53,6 @@ app.get('/test', keycloak.protect(), function(req, res,next) {
     res.send({"message": "This is a test API"});
 });
 
-// app.get('/logoff', keycloak.protect(), (req, res) => {
-//     console.log('logout clicked');
-//     res.send('http://' + HOST + ':3000/logout');
-// });
-
-// app.get("/signout", (req, res, next) => {
-// 	console.log('Attempting to logout');
-// 	req.logout();
-// 	req.session.destroy();
-// 	res.redirect('/');
-// });
-
 app.post('/addClip', keycloak.protect(), (req, res) => {
     /*  
     Request body format: 
@@ -77,58 +66,56 @@ app.post('/addClip', keycloak.protect(), (req, res) => {
     let token = req.headers.authorization.split("Bearer")[1].trim();
     let decodedToken = jwtDecode(token);
     let userId = decodedToken["preferred_username"];
-    // userId = "adam@gmail.com"
-    // console.log(userId);
     console.log('Got body:', req.body);
     let dbo = mongodb.db(dbName);
     myobj = req.body;
     myobj["userId"] = userId
-    // let userId = reqObj["userId"];
-    // let clipData = reqObj["clipboardText"]
-    // let dbo = mongodb.db(dbName);
-    // myobj = reqObj;
-    dbo.collection("clips").insertOne(myobj, function(err, res) {
+    dbo.collection("clips").insertOne(myobj, function(err, dbResult) {
         if (err) throw err;
         console.log("1 document inserted");
         io.sockets.in("room-"+userId).emit("newData", myobj);      
-    });
-    res.send(200);
+        let result = {}
+        result["success"] = true
+        result["message"] = "Successfully added the clipboard text"
+        res.send(result);
+    });    
 });
 
 app.get('/clips/:userId', keycloak.protect(), function(req, res) {
-
-    // let token = req.headers.authorization.split("Bearer")[1].trim();
-    // let decodedToken = jwtDecode(token);
-
-    // let userId = decodedToken["preferred_username"];
     userId = req.params["userId"];
     console.log("clips api: ", userId);
-    // main_result = {};
+    
     let dbo = mongodb.db(dbName);
     let query = {userId: userId};
-    dbo.collection("clips").find(query).toArray(function(err, result) {
-      if (err) throw err;
-    //   main_result = result;
-      res.send(result);
+    dbo.collection("clips").find(query).toArray(function(err, dbResult) {
+      if (err) throw err;         
+      res.send(dbResult);
     });
 });
 
-// app.route('/deleteClip').delete(function(req, res) {
-//     console.log('Got body DELETE:', req.body);
-//     let result = {};
-//     let dbo = mongodb.db(dbName);
-//     id = req.body._id;
-//     console.log("Id to be deleted: "+id)
-//     let myquery = { _id: new mongodb.ObjectID(id) };
-//     dbo.collection("clips").deleteOne(myquery, function(err, res) {
-//         if (err) throw err;
-//         result["success"] = true
-//         result["message"] = "Successfully deleted the document"
-//         res.status(200);
-//         res.send(result);
-//     });
-    
-// });
+app.route('/deleteClip').delete(function(req, res) {
+    console.log('Got body DELETE:', req.body);
+    let result = {};
+    let dbo = mongodb.db(dbName);
+    id = req.body["_id"];
+    console.log("Id to be deleted: "+id)
+    let myquery = { _id: new ObjectId(id) };
+    dbo.collection("clips").deleteOne(myquery, function(err, dbResult) {
+        console.log();
+        if (err) throw err;
+        let result = {};
+        
+        if(dbResult.result.n > 0) {
+            result["success"] = true
+            result["message"] = "Successfully deleted the clipboard text"        
+        } else {
+            result["success"] = false
+            result["message"] = "Document not found"        
+        }
+        // res.status(200);
+        res.send(result);
+    });    
+});
 
 // let socketController = new SocketController();
 
@@ -147,20 +134,27 @@ io.on('connection', function(socket) {
                 },
                json: true
             }
-            const request = require('request');
-            request(options, (err, res, body) => {
-                if (err) { return console.log(err); }
-                if("error" in body) {
-                    console.log("Invalid token");
-                    socket.emit({success: false})
-                } else {
-                    let decodedToken = jwtDecode(token);
-                    let userId = decodedToken["preferred_username"];
-                    console.log("User joined: ",userId)
-                    socket.join("room-"+userId);   
-                    socket.emit({success: true})                                     
-                }
-            });
+            // const request = require('request');
+            // request(options, (err, res, body) => {
+            //     if (err) { return console.log(err); }
+            //     if("error" in body) {
+            //         console.log("Invalid token");
+            //         socket.emit({success: false})
+            //     } else {
+            //         let decodedToken = jwtDecode(token);
+            //         let userId = decodedToken["preferred_username"];
+            //         console.log("User joined: ",userId)
+            //         socket.join("room-"+userId);   
+            //         socket.emit({success: true})                                     
+            //     }
+            // });
+
+
+            let decodedToken = jwtDecode(token);
+            let userId = decodedToken["preferred_username"];
+            console.log("User joined: ",userId)
+            socket.join("room-"+userId);   
+            socket.emit({success: true})    
         }   
     });
     socket.on("leave", function(reqObj) {
@@ -172,39 +166,8 @@ io.on('connection', function(socket) {
         socket.emit({
             message:'you are removed from the room '
         });
-    });
-    // socket.on('desktopClient',function(reqObj){
-    //     let userId = reqObj["userId"];
-    //     console.log("user id:", userId);
-    //     let clipData = reqObj["clipboardText"]
-    //     let dbo = mongodb.db(dbName);
-    //     myobj = reqObj;
-    //     dbo.collection("clips").insertOne(myobj, function(err, res) {
-    //         if (err) throw err;
-    //         // console.log(res);
-    //         console.log("1 document inserted");      
-    //         // console.log("socket info: ", io.sockets.manager.roomClients[socket.id]);
-    //         io.sockets.in("room-"+userId).emit("newData", myobj);      
-    //     });
-    //     //   socket.to('mobileClient').emit(clipData,'From Desktop');
-    // });
-    
-    // socket.on('mobileClient',function(reqObj){
-    //     let userId = reqObj["userId"];
-    //     let clipData = reqObj["clipboardText"]
-    //     let dbo = mongodb.db(dbName);
-    //     myobj = reqObj;
-    //     dbo.collection("clips").insertOne(myobj, function(err, res) {
-    //         if (err) throw err;
-    //         console.log("1 document inserted");
-    //         io.sockets.in("room-"+userId).emit("newData", myobj);           
-    //     });
-    //     // socket.emit('message','From Desktop');
-    //     // socket.to(userId).emit("newData", clipData);
-    // });
-        
+    });    
 });
 
-// server.listen(3000);
 server.listen(PORT)
 console.log('HTTP Server listening on: %s', PORT);
